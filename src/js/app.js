5715d96400567a5470daf349d7ae3f1277ab6632
roowix <gerasimenko_andrei@bk.ru>
2022-01-18 14:11:59 +0700

    project login, part 3, server request

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import "../css/style.css";
 import UI from "./config/ui.config";
 import { validate } from "./helpers/validate";
 import { showInputError, removeInputError } from "./views/form";
+import { login } from "./services/auth.service";
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -20,7 +21,7 @@ inputs.forEach((el) => {
 });
 
 // Handlers
-function onSubmit() {
+async function onSubmit() {
   const isValidForm = inputs.every((el) => {
     const isValidInput = validate(el);
 
@@ -31,5 +32,10 @@ function onSubmit() {
     return isValidInput;
   });
 
-  console.log(isValidForm);
+  if (!isValidForm) return;
+
+  try {
+    await login(inputEmail.value, inputPassword.value);
+    form.reset();
+  } catch (e) {}
 }
